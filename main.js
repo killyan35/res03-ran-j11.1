0cb1e127d0f8495ba53ef3cb76e6b4df311d2ebe
killyan35 <kilyan.gerard@3wa.io>
2022-11-22 15:28:35 +0000

    l'idée du siecle a marché

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
 let form = document.querySelector("form:first-of-type");
-console.log(form);
 form.addEventListener("submit", function(e){
     e.preventDefault(); 
     let usernameInput=document.querySelector("#username");
     let emailInput=document.querySelector("#email");
-    console.log(usernameInput);
-    let formOK=true;
-    if (usernameInput.value !=="")
+    let passwordInput=document.querySelector("#password");
+    let confirmpasswordInput=document.querySelector("#confirm-password");
+    
+    
+    if (usernameInput.value&&emailInput.value&&passwordInput.value&&confirmpasswordInput !=="")
     {
         e.target.submit();
         
-    }
-    else if (usernameInput.value =="")
+    }else{
+    
+     if (usernameInput.value ==="")
     {
     
         let inputError = document.createElement("p");
-        
-        
         let fieldset = document.querySelector("form:first-of-type fieldset:first-of-type");
         inputError.innerHTML="Veuillez choisir un nom d'utilisateur";
         inputError.classList.add("error");
         usernameInput.classList.add("error");
         fieldset.appendChild(inputError);
-        formOK=false;
+        
     }
-    else if   (emailInput.value !=="")
-    {
-        e.target.submit();
-    }
-    else
+    
+     if (emailInput.value ==="")
     {
         let inputError2 = document.createElement("p");
         let fieldset2= document.querySelector("form:first-of-type fieldset:nth-of-type(2)");
@@ -36,8 +33,37 @@ form.addEventListener("submit", function(e){
         inputError2.classList.add("error");
         emailInput.classList.add("error");
         fieldset2.appendChild(inputError2);
-        formOK=false;
+        
     }
+    
+    if (passwordInput.value ==="")
+    {
+        let inputError3 = document.createElement("p");
+        let fieldset3= document.querySelector("form:first-of-type fieldset:nth-of-type(3)");
+        inputError3.innerHTML="Aucun mot de passe saisi.";
+        inputError3.classList.add("error");
+        passwordInput.classList.add("error");
+        fieldset3.appendChild(inputError3);
+        
+    }
+    
+    if (confirmpasswordInput.value ==="")
+    {
+        let inputError4 = document.createElement("p");
+        let fieldset4= document.querySelector("form:first-of-type fieldset:nth-of-type(4)");
+        inputError4.innerHTML=" Les deux mots ne passe ne correspondent pas.";
+        inputError4.classList.add("error");
+        confirmpasswordInput.classList.add("error");
+        fieldset4.appendChild(inputError4);
+       
+    }
+    }
+    
 });
+let form1 = document.querySelector("form:nth-of-type(2)");
+form1.addEventListener("submit", function(e){
+    e.preventDefault(); 
+    let studiesInput=document.querySelector("#studies-bep");
+    let Input=document.querySelector("#email");
 
-
+});
